b239d554485db51c3ce4dd6ac577126dbc9f441f
icydoge <icydoge@gmail.com>
2017-05-25 01:13:16 +0100

    Initial site.

---
+++ static/index.js
@@ -0,0 +1,31 @@
+function setDescriptor(descriptorID, describedObject) {
+    $('.descriptor').html('');
+    $('.project-image').removeClass('project-image-highlighted');
+    $('#' + descriptorID).html("<b>" + $(describedObject).attr('data-name') + "</b>: " + $(describedObject).attr('data-description'));
+    if ($(describedObject).attr('data-link') !== "") {
+        $('#' + descriptorID).html($('#' + descriptorID).html() + "<a class='read-more' href='" + $(describedObject).attr('data-link') + "' target='_blank'> (More)</a>" );
+    }
+    $(describedObject).addClass('project-image-highlighted');
+}
+
+var first_project = $('#PROJECTBLOCK').children('.descriptor-block').first().children('img').first();
+setDescriptor('descriptor-a', first_project);
+
+$(".project-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#PROJECTS").offset().top
+    }, 1000);
+});
+
+$(".project-image").click(function() {
+    var nearest_descriptor = $(this).parent().find('.descriptor').first().attr('id');
+    setDescriptor(nearest_descriptor, this);
+    $(".language-icon-highlight").removeClass('language-icon-highlight');
+    $(".language-icon:not(grayscaled)").addClass('grayscaled');
+    var languages = $(this).attr('data-use-langs').split(',');
+    for (var i = 0, len = languages.length; i < len; i++) {
+        $('.language-icon[data-lang=' + languages[i] +']').removeClass('grayscaled');
+        $('.language-icon[data-lang=' + languages[i] +']').addClass('language-icon-highlight');
+    }
+    
+})
